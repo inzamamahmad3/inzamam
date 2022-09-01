563e413f738c33f6efa2bfec845e91a58c17b89e
Inzamam Ahmad <enzee274@gmail.com>
2022-08-31 21:55:59 -0700

    First Version

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+Inzamam Ahmad 
+Software Engineer
+Full Stack JavaScript Developer. Ready to tackle real problems. Ehtereum Blockchain developer
+Social Links
+twitter  :  https://twitter.com/IInzamamAhmad
+github   :  https://github.com/inzamamahmad3
+`);
